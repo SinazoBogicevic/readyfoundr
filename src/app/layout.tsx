acefd13958147ffc6b3c6f0b5c41d11de96a2a97
Sinazo <snoqhamza@yahoo.com>
2025-02-24 10:40:37 +0100

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
   title: "Ready Foundr",
-  description: "Building MVPs for non-tech founders",
+  description:
+    "Ready Foundr helps non-technical founders build successful MVPs quickly and efficiently. Turn your vision into reality with our expert development services.",
 };
 
 export default function RootLayout({
@@ -12,6 +14,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5Q4DJ0F9T8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5Q4DJ0F9T8');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
